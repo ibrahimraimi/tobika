bc0fe508085228d0834d437becd1b70400ad4fbf
Ibrahim Raimi <raimiibrahim44@gmail.com>
2025-11-26 01:14:09 +0100

    feat: Introduce core types and utilities for media, users, and internationalization, and add essential dependencies.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-import Link from 'next/link'
-import { Button } from '@/components/ui/button'
-import { SiteHeader } from '@/components/site-header'
-import { Fragment } from 'react'
-
 export default function Home() {
   return <main>Tobika</main>
 }
